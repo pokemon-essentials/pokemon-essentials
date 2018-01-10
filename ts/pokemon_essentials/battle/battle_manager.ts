8b09b766f2b2b5c0fde525e5a4d6994d643f1cec
andarms <andarms@outlook.com>
2018-01-10 11:40:01 -0500

    Enhace event BattleManager queue

--- ts/pokemon_essentials/battle/battle_manager.ts
+++ ts/pokemon_essentials/battle/battle_manager.ts
@@ -6,27 +6,23 @@ namespace PE.Battle {
   interface IBattleEvent { name: string, params: any[] };
 
   export const enum Phase { None, Init, ActionSelection, MoveSelection };
+  export const enum WaitMode { None, Animation };
 
   export abstract class Manager {
-    private static _queue: any[];
+    private static _queue = [];
 
     static battlers: PE.Battle.Battler[] = [];
     static field: ActiveField;
-    static phase: Phase;
-    static turncount: number;
-    static waitMode: string;
 
+    static phase = PE.Battle.Phase.None;
+    static turncount = 0;
+    static waitMode = WaitMode.None;
     static weather = PE.Weather.None;
 
     static setup() {
-      this._queue = [];
       this.phase = Phase.Init;
-      this.waitMode = "";
-      this.turncount = 0;
-
       // this.side = new PE.Battle.ActiveField();
       this.field = new PE.Battle.ActiveField();
-      this.weather = PE.Weather.None;
 
       // this.battlers = []; // TODO
       for (const battler of this.battlers) {
@@ -40,57 +36,47 @@ namespace PE.Battle {
     }
 
     static isBusy() {
-      return $gameMessage.isBusy() || this.waitMode !== "";
+      return $gameMessage.isBusy() || this.waitMode !== WaitMode.None;
     }
 
-    static push(name, ...args: any[]) {
-      let params = Array.prototype.slice.call(arguments, 1);
-      this._queue.push({ name: name, params: params });
+    static push(method) {
+      this._queue.push(method);
     }
 
     static pop() {
       if (this._queue.length <= 0) return;
       let method = this._queue.shift();
-      if (!method.name || !this[method.name]) throw new Error('Method not found: ' + method.name);
-      this[method.name].apply(this, method.params);
-    }
-
-    private static __showMessage(msg: string) {
-      while (msg.length > CHARACTERS_PER_LINE) {
-        let line = msg.substring(0, CHARACTERS_PER_LINE);
-        let truncateIndex = Math.min(line.length, line.lastIndexOf(" "));
-        line = line.substring(0, truncateIndex);
-        $gameMessage.add(line + '\\n');
-        msg = msg.substring(truncateIndex);
-      }
-      $gameMessage.add(msg + '\\|\\^');
-    }
-
-    private static __showPausedMessage(msg: string) {
-      while (msg.length > CHARACTERS_PER_LINE) {
-        let line = msg.substring(0, CHARACTERS_PER_LINE);
-        let truncateIndex = Math.min(line.length, line.lastIndexOf(" "));
-        line = line.substring(0, truncateIndex);
-        $gameMessage.add(line + '\\n');
-        msg = msg.substring(truncateIndex);
-      }
-      $gameMessage.add(msg);
+      method.apply(this);
     }
 
     static showMessage(msg: string) {
-      this.push('__showMessage', msg);
+      this.push(() => {
+        while (msg.length > CHARACTERS_PER_LINE) {
+          let line = msg.substring(0, CHARACTERS_PER_LINE);
+          let truncateIndex = Math.min(line.length, line.lastIndexOf(" "));
+          line = line.substring(0, truncateIndex);
+          $gameMessage.add(line + '\\n');
+          msg = msg.substring(truncateIndex);
+        }
+        $gameMessage.add(msg + '\\|\\^');
+      });
     }
 
     static showPausedMessage(msg: string) {
-      this.push('__showPausedMessage', msg);
-    }
-
-    private static __changePhase(phase: Phase) {
-      this.phase = phase;
+      this.push(() => {
+        while (msg.length > CHARACTERS_PER_LINE) {
+          let line = msg.substring(0, CHARACTERS_PER_LINE);
+          let truncateIndex = Math.min(line.length, line.lastIndexOf(" "));
+          line = line.substring(0, truncateIndex);
+          $gameMessage.add(line + '\\n');
+          msg = msg.substring(truncateIndex);
+        }
+        $gameMessage.add(msg);
+      });
     }
 
     static changePhase(phase: Phase) {
-      this.push('__changePhase', phase);
+      this.push(() => this.phase = phase);
     }
 
   }
