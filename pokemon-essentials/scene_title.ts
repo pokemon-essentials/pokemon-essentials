4bd208f9625c37537ec71229f75ecb679ae9b77d
andarms <andarms@outlook.com>
2018-06-25 22:20:14 -0500

    Fix no implicity TS error

--- pokemon-essentials/scene_title.ts
+++ pokemon-essentials/scene_title.ts
@@ -1,4 +1,3 @@
-
 namespace PE.TitleScenes {
   let StartPressed = false;
 
@@ -27,10 +26,10 @@ namespace PE.TitleScenes {
 
     update() {
       super.update();
-      if ((Input.isTriggered('ok') || TouchInput.isTriggered()) && !this.isBusy()) {
+      if ((Input.isTriggered("ok") || TouchInput.isTriggered()) && !this.isBusy()) {
         StartPressed = true;
       }
-      if ((Input.isTriggered('cancel') || TouchInput.isCancelled()) && !this.isBusy()) {
+      if ((Input.isTriggered("cancel") || TouchInput.isCancelled()) && !this.isBusy()) {
         StartPressed = false;
       }
       if (StartPressed && !this.isBusy()) {
@@ -38,10 +37,10 @@ namespace PE.TitleScenes {
       } else {
         this._commandWindow.close();
       }
-      this.sprites['start'].visible = !StartPressed;
-      this.sprites['bgPattern'].origin.x += 1;
-      this.sprites['effect1'].rotation += 0.005;
-      this.sprites['effects2'].rotation += 0.01;
+      this.sprites["start"].visible = !StartPressed;
+      this.sprites["bgPattern"].origin.x += 1;
+      this.sprites["effect1"].rotation += 0.005;
+      this.sprites["effects2"].rotation += 0.01;
     }
 
     isBusy() {
@@ -54,45 +53,45 @@ namespace PE.TitleScenes {
     }
 
     createBackground() {
-      this.sprites['bg'] = new Sprite(ImageManager.loadTitle1('custom_background'));
-      this.addChild(this.sprites['bg']);
+      this.sprites["bg"] = new Sprite(ImageManager.loadTitle1("custom_background"));
+      this.addChild(this.sprites["bg"]);
 
-      this.sprites['bgPattern'] = new TilingSprite(ImageManager.loadPicture('custom_plane'));
-      this.sprites['bgPattern'].move(0, 0, Graphics.width, Graphics.height);
-      this.addChild(this.sprites['bgPattern']);
+      this.sprites["bgPattern"] = new TilingSprite(ImageManager.loadPicture("custom_plane"));
+      this.sprites["bgPattern"].move(0, 0, Graphics.width, Graphics.height);
+      this.addChild(this.sprites["bgPattern"]);
 
-      this.sprites['clouds2'] = new Sprite(ImageManager.loadTitle1('custom_clouds_2'));
-      this.addChild(this.sprites['clouds2']);
+      this.sprites["clouds2"] = new Sprite(ImageManager.loadTitle1("custom_clouds_2"));
+      this.addChild(this.sprites["clouds2"]);
 
-      this.sprites['effect1'] = new Sprite(ImageManager.loadTitle1('custom_effect'));
-      this.sprites['effect1'].x = Graphics.width / 2;
-      this.sprites['effect1'].y = 325;
-      this.sprites['effect1'].anchor.x = 0.5;
-      this.sprites['effect1'].anchor.y = 0.5;
-      this.addChild(this.sprites['effect1']);
+      this.sprites["effect1"] = new Sprite(ImageManager.loadTitle1("custom_effect"));
+      this.sprites["effect1"].x = Graphics.width / 2;
+      this.sprites["effect1"].y = 325;
+      this.sprites["effect1"].anchor.x = 0.5;
+      this.sprites["effect1"].anchor.y = 0.5;
+      this.addChild(this.sprites["effect1"]);
 
-      this.sprites['effects2'] = new Sprite(ImageManager.loadTitle1('gen_6_effect2'));
-      this.sprites['effects2'].x = Graphics.width / 2;
-      this.sprites['effects2'].y = 325;
-      this.sprites['effects2'].anchor.x = 0.5;
-      this.sprites['effects2'].anchor.y = 0.5;
-      this.addChild(this.sprites['effects2']);
+      this.sprites["effects2"] = new Sprite(ImageManager.loadTitle1("gen_6_effect2"));
+      this.sprites["effects2"].x = Graphics.width / 2;
+      this.sprites["effects2"].y = 325;
+      this.sprites["effects2"].anchor.x = 0.5;
+      this.sprites["effects2"].anchor.y = 0.5;
+      this.addChild(this.sprites["effects2"]);
 
-      this.sprites['clouds1'] = new Sprite(ImageManager.loadTitle1('custom_clouds_1'));
-      this.addChild(this.sprites['clouds1']);
+      this.sprites["clouds1"] = new Sprite(ImageManager.loadTitle1("custom_clouds_1"));
+      this.addChild(this.sprites["clouds1"]);
 
-      this.sprites['bars'] = new Sprite(ImageManager.loadTitle1('custom_bars'));
-      this.addChild(this.sprites['bars']);
+      this.sprites["bars"] = new Sprite(ImageManager.loadTitle1("custom_bars"));
+      this.addChild(this.sprites["bars"]);
 
-      this.sprites['start'] = new PE.Sprites.Blinker(20);
-      this.sprites['start'].bitmap = ImageManager.loadTitle1('start');
-      this.sprites['start'].y = 362;
-      this.addChild(this.sprites['start']);
+      this.sprites["start"] = new PE.Sprites.Blinker(20);
+      this.sprites["start"].bitmap = ImageManager.loadTitle1("start");
+      this.sprites["start"].y = 362;
+      this.addChild(this.sprites["start"]);
     }
 
     createForeground() {
       this._pokelogo = new Sprite();
-      this._pokelogo.bitmap = ImageManager.loadTitle1('pokelogo');
+      this._pokelogo.bitmap = ImageManager.loadTitle1("pokelogo");
       this._pokelogo.x = Graphics.width / 2;
       this._pokelogo.y = 24;
       this._pokelogo.anchor.x = 0.5;
@@ -101,57 +100,57 @@ namespace PE.TitleScenes {
 
     createCommandWindow() {
       this._commandWindow = new CustomTitleCommandWindow();
-      this._commandWindow.setHandler('battle', this.commandNewGame.bind(this));
-      this._commandWindow.setHandler('map', this.commandMap.bind(this));
-      this._commandWindow.setHandler('continue', this.commandContinue.bind(this));
-      this._commandWindow.setHandler('options', this.commandOptions.bind(this));
+      this._commandWindow.setHandler("battle", this.commandNewGame.bind(this));
+      this._commandWindow.setHandler("map", this.commandMap.bind(this));
+      this._commandWindow.setHandler("continue", this.commandContinue.bind(this));
+      this._commandWindow.setHandler("options", this.commandOptions.bind(this));
       this.addWindow(this._commandWindow);
-    };
+    }
 
     commandNewGame() {
       DataManager.setupNewGame();
       this._commandWindow.close();
       this.fadeOutAll();
       SceneManager.goto(PE.Battle.Scene_Battle);
-      // $Player.party = PE.Pokemon.getRandomParty(6);
-      $Player.party = [new Pokemon.Pokemon(Pokedex.GROUDON, 100)];
+      $Player.party = PE.Pokemon.getRandomParty(6);
+      // $Player.party = [new Pokemon.Pokemon(Pokedex.GROUDON, 100)];
       $Battle.setup([Trainers.RandomTrainer()], []);
       $Battle.start();
-    };
+    }
 
     commandMap() {
       DataManager.setupNewGame();
       this._commandWindow.close();
       this.fadeOutAll();
       SceneManager.goto(Scene_Map);
-    };
+    }
 
     commandContinue() {
       this._commandWindow.close();
       SceneManager.push(Scene_Load);
-    };
+    }
 
     commandOptions() {
       this._commandWindow.close();
       SceneManager.push(Scene_Options);
-    };
+    }
 
     playTitleMusic() {
       // AudioManager.playBgm($dataSystem.titleBgm);
       AudioManager.stopBgs();
       AudioManager.stopMe();
-    };
+    }
   }
 
   export class CustomTitleCommandWindow extends Window_TitleCommand {
     makeCommandList() {
-      this.addCommand(i18n._('Wild Battle'), 'battle');
-      this.addCommand(i18n._('Map'), 'map');
-      this.addCommand(i18n._('Options'), 'options');
+      this.addCommand(i18n._("Wild Battle"), "battle", true);
+      this.addCommand(i18n._("Map"), "map", true);
+      this.addCommand(i18n._("Options"), "options", true);
     }
 
     updatePlacement() {
-      this.x = (Graphics.boxWidth - this.width);
+      this.x = Graphics.boxWidth - this.width;
       this.y = Graphics.boxHeight - this.height;
     }
     windowWidth() {
@@ -163,13 +162,13 @@ namespace PE.TitleScenes {
     makeCommandList() {
       for (const pokemon in Pokedex) {
         if (Pokedex.hasOwnProperty(pokemon)) {
-          this.addCommand(pokemon, 'pokemon');
+          this.addCommand(pokemon, "pokemon", true);
         }
       }
     }
 
     updatePlacement() {
-      this.x = (Graphics.boxWidth - this.width);
+      this.x = Graphics.boxWidth - this.width;
       this.y = Graphics.boxHeight - this.height;
     }
 
@@ -182,7 +181,7 @@ namespace PE.TitleScenes {
   }
 }
 
-Scene_Boot.prototype.start = function () {
+Scene_Boot.prototype.start = function() {
   Scene_Base.prototype.start.call(this);
   SoundManager.preloadImportantSounds();
   if (DataManager.isBattleTest()) {
@@ -199,6 +198,3 @@ Scene_Boot.prototype.start = function () {
   }
   this.updateDocumentTitle();
 };
-
-
-
